ebbceb0beda7ad906416ccf16587cf503da1ac14
ipaturin <paturin.ilia@yandex.ru>
2024-06-25 00:08:04 +0300

    feat(linter): add linter config

---
+++ settings.gradle.kts
@@ -0,0 +1,42 @@
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+rootProject.name = "NewsSearchApp"
+include(":app")
+
+include(":core:opennews-api")
+include(":core:database")
+include(":core:data")
+include(":core:common")
+include(":core:uikit")
+
+include(":features:news-main:ui")
+include(":features:news-main:ui-logic")
+
+include(":baselineprofile")
