8890e125c6f33f8451e3e821c45e08ddc6a32ee6
ipaturin <paturin.ilia@yandex.ru>
2024-06-25 00:09:08 +0300

    feat(core): add room database

---
+++ core/database/build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.androidx.room)
+}
+
+android {
+    namespace = "dev.androidbroadcast.news.database"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 24
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+room {
+    schemaDirectory("${rootProject.projectDir}/schemas")
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+}
